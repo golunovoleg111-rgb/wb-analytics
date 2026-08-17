c69f6b4cf5fafa77e898f2d6a46e45391acd83f7
golunovoleg111-rgb <golunovoleg111@gmail.com>
2026-08-17 11:48:22 +0300

    Expose LAN controls to desktop UI

--- desktop/preload.js
+++ desktop/preload.js
@@ -4,4 +4,7 @@ contextBridge.exposeInMainWorld('BJobDesktop', {
   getInfo: () => ipcRenderer.invoke('desktop-info'),
   exportJson: (snapshot) => ipcRenderer.invoke('desktop-export-json', snapshot),
   importJson: () => ipcRenderer.invoke('desktop-import-json'),
+  lanStart: () => ipcRenderer.invoke('lan-start'),
+  lanStop: () => ipcRenderer.invoke('lan-stop'),
+  lanStatus: () => ipcRenderer.invoke('lan-status'),
 });
