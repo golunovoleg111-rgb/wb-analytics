955e568a9b2658a66997b540613aaee49d4b0cd4
golunovoleg111-rgb <golunovoleg111@gmail.com>
2026-08-17 11:44:50 +0300

    Expose secure desktop backup bridge

--- desktop/preload.js
+++ desktop/preload.js
@@ -2,4 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('BJobDesktop', {
   getInfo: () => ipcRenderer.invoke('desktop-info'),
+  exportJson: (snapshot) => ipcRenderer.invoke('desktop-export-json', snapshot),
+  importJson: () => ipcRenderer.invoke('desktop-import-json'),
 });
